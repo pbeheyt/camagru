d589f74df4e24763be38dab982ecb19cf5992f8d
Pierre BEHEYT <pierre.beheyt.bba@edhec.com>
2024-06-14 05:03:34 +0200

    add scpecific navbar header for auth page

--- public/js/include-auth.js
+++ public/js/include-auth.js
@@ -1,14 +1,11 @@
-// Wait until the entire HTML document is fully loaded and parsed
 document.addEventListener('DOMContentLoaded', function() {
-  
+
 	function loadContent(url, elementId) {
 	  fetch(url)
 		.then(function(response) {
-		  // Convert the response into text
 		  return response.text();
 		})
 		.then(function(data) {
-		  // Insert the fetched content into the specified DOM element
 		  document.getElementById(elementId).innerHTML = data;
 		})
 		.catch(function(error) {
@@ -16,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
 		});
 	}
   
-	loadContent('/html/header.html', 'header');
+	loadContent('/html/header-no-navbar.html', 'header');
 	loadContent('/html/footer.html', 'footer');
   });
   
---
+++ public/js/include-main.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+	function loadContent(url, elementId) {
+	  fetch(url)
+		.then(function(response) {
+		  return response.text();
+		})
+		.then(function(data) {
+		  document.getElementById(elementId).innerHTML = data;
+		})
+		.catch(function(error) {
+		  console.error('Error fetching the content:', error);
+		});
+	}
+  
+	loadContent('/html/header.html', 'header');
+	loadContent('/html/footer.html', 'footer');
+  });
+  
